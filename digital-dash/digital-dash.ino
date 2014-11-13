0a837c91c31631748945965a53f3d861276cc00d
David T <theo8ald@gmail.com>
2014-11-13 21:43:03 +0000

    Added air temp

--- digital-dash/digital-dash.ino
+++ digital-dash/digital-dash.ino
@@ -6,16 +6,19 @@
 // include the library code:
 #include <LiquidCrystal.h>
 
-
 const int ledCount = 14;    // the number of digital pin LEDs in the bar graph
 int ledPins[] = { 
   41, 43, 45, 47, 49, 51, 53, 40, 42, 44, 46, 48, 50, 52 };   // an array of digital pin numbers to which LEDs are attached
+int airsensorpin = 3;  
 int oldscreen = 0;
 int screen = 0;
-int sampleperiod = 250;
-unsigned long sampleold = 0;
-int logperiod = 1000;
-unsigned long logold = 0;
+long sampleperiod = 250;
+long sampleold = 0;
+long logperiod = 1000;
+long logold = 0;
+long displayperiod = 500;
+long displayold = 0;
+unsigned long currentmillis;
 int watertemp = 0;
 int watertempmin = 0;
 int watertempmax = 0;
@@ -24,14 +27,14 @@ int oiltemp = 0;
 int oiltempmin = 0;
 int oiltempmax = 0;
 int oiltempmaxalarm = 100;
-int airtemp = 0;
-int airtempmin = 0;
-int airtempmax = 0;
-unsigned long battvolt = 0;
-unsigned long battvoltmin = 0;
-unsigned long battvoltmax = 0;
-unsigned long battvoltminalarm = 130;    // voltage *10 to avoid using float arithmetic 
-unsigned long battvoltmaxalarm = 145;    // voltage *10 to avoid using float arithmetic 
+float airtemp = 0;
+float airtempmin = 0;
+float airtempmax = 0;
+float battvolt = 0;
+float battvoltmin = 0;
+float battvoltmax = 0;
+float battvoltminalarm = 130;
+float battvoltmaxalarm = 145; 
 int g = 0;
 int gmin = 0;
 int gmax = 0;
@@ -49,190 +52,247 @@ boolean strobeleds = false;
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(36, 34, 32, 30, 28, 26);
 
-void setup() {
-
- attachInterrupt(0, tachpulse, RISING);  
+void setup()
+{
+  Serial.begin(9600);
+  attachInterrupt(0, tachpulse, RISING);  
   
 // loop over the pin array and set them all to output:
-  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+  for (int thisLed = 0; thisLed < ledCount; thisLed++)
+  {
     pinMode(ledPins[thisLed], OUTPUT);
   }
   lcd.begin(16, 2);
   lcdstartup();        // display welcome message
   ledstartup();        // sweep leds
-  delay(500);
 }
 
-void loop() {
+void loop()
+{
   
   // refresh parameters
-  if (millis() > (sampleold + sampleperiod)); {
+  currentmillis = millis();
+  if (currentmillis - sampleold > sampleperiod)
+  {
+    sampleold = millis();
     //  readwatertemp();
     //  readoiltemp();
-    //  readairtemp();
+    airtemp = readairtemp();
     battvolt = readbatteryvolt();
     //  readaccelerometer();
     readrpm();
   }
   
   // log parameters
-  if (millis() > (logold + logperiod)) {
+  currentmillis = millis();
+  if (currentmillis - logold > logperiod)
+  {
     logtosdcard();
+    logold = millis();
   }  
 
   // refresh screen
-  if (screen == 0) {
-    displayscreenhome();
-  } 
-  else if (screen == 1) {
-    displayscreenwater();
+  currentmillis = millis();
+  if (currentmillis - displayold > displayperiod)
+    {
+    displayold = millis();
+    if (screen == 0)
+    {
+      Serial.println("OK2"); 
+      displayscreenhome();
+    }
+    else if (screen == 1)
+    {
+      displayscreenwater();
+    }
+    else if (screen == 2)
+    {
+      displayscreenoil();
+    }
+    else if (screen == 3)
+    {
+      displayscreenair();
+    }
+    else if (screen == 4)
+    {
+      displayscreenbatt();
+    }
+    else if (screen == 5)
+    {
+      displayscreenaccel();
+    }
+    else if (screen == 6)
+    {
+      displayscreenrpm();
+    }
+    else if (screen == 7)
+    {
+      displayscreenalarm();
+    }
   }
-  else if (screen == 2) {
-    displayscreenoil();
-  }
-  else if (screen == 3) {
-    displayscreenair();
-  }
-  else if (screen == 4) {
-    displayscreenbatt();
-  }
-  else if (screen == 5) {
-    displayscreenaccel();
-  }
-  else if (screen == 6) {
-    displayscreenrpm();
-  }
-  else if (screen == 7) {
-    displayscreenalarm();
-  }
+  
   //refresh leds
-  if (alarmreason != 0) {  // light all leds for alarm
-    displayleds (65535);
-  }  
-  else {
+//  if (alarmreason != 0) {  // light all leds for alarm
+//    displayleds (65535);
+//  }  
+//  else {
     
-    if (rpm > rpmredline) {  // strobe leds for redline rpm
+//    if (rpm > rpmredline) {  // strobe leds for redline rpm
 
-      if ((strobeleds == false) && (millis() > (strobeledsold + strobeledsperiod))) {
-        displayleds (0);
-        strobeleds == true;
-      }
-      else if ((strobeleds == true) && (millis() > (strobeledsold + strobeledsperiod))) {
-        displayleds (65535);
-        strobeleds == false;
-      }
+//      if ((strobeleds == false) && (millis() > (strobeledsold + strobeledsperiod))) {
+//        displayleds (0);
+//        strobeleds == true;
+//      }
+//      else if ((strobeleds == true) && (millis() > (strobeledsold + strobeledsperiod))) {
+//        displayleds (65535);
+//        strobeleds == false;
+//      }
 
-    else {
-      displayleds (rpm);
-    }
-    }
+//    else {
+//      displayleds (rpm);
+//    }
+//    }
     
-  }    
+//  }    
 }  // end of main loop
 
-void lcdstartup() {
+void lcdstartup()
+{
   lcd.setCursor(0, 0);
   lcd.print("*  TIGER AVON  *");
   lcd.setCursor(0, 1);
   lcd.print("*  2.0 ZETEC   *");
-return;
+  return;
 }
 
-void ledstartup() {
-  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+void ledstartup()
+{
+  for (int thisLed = 0; thisLed < ledCount; thisLed++)
+  {
     digitalWrite(ledPins[thisLed], HIGH);
     delay(30); 
   }
-   for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+  for (int thisLed = 0; thisLed < ledCount; thisLed++)
+  {
    digitalWrite(ledPins[thisLed], LOW);
    delay(30); 
   }
-return;
+  return;
 }
 
-unsigned long readbatteryvolt() {            // large ints used for calculation to avoid using floats
-  unsigned long battvalue = analogRead(A0);  // read the input on analoge pin 0
-  battvalue = battvalue * 48 ;               // convert to a voltage (0-5V) multiplied by 10 (my voltage regulator outputs 4.8V)
-  battvalue = battvalue * 675;               // multiply up by ratio of voltage divider resistors
-  battvalue = battvalue / 216;   
-  battvalue = battvalue / 1023;              // 1023 represents power supply, i.e. 4.8V
-  battvalue = battvalue + 5;                 // add back in 0.5V voltage drop due to diode on 12V input
-  if (battvalue < battvoltmin) {
+float readbatteryvolt()
+{
+  int reading = analogRead(A0);         // read the input on analoge pin 0
+  float battvalue = reading * 4.8 ;     // convert to a voltage (0-5V) (my voltage regulator outputs 4.8V)
+  battvalue = battvalue * 3.102;        // multiply up by ratio of voltage divider resistors (670 / 216 = 3.102)
+  battvalue = battvalue / 1024;         // 1024 represents power supply, i.e. 4.8V
+  battvalue = battvalue + 0.5;          // add back in 0.5V voltage drop due to diode on 12V input
+  if (battvalue < battvoltmin)
+  {
     battvoltmin = battvalue;
   }  
-  if (battvalue > battvoltmax) {
+  if (battvalue > battvoltmax)
+  {
     battvoltmax = battvalue;
   }  
-  if (battvalue > battvoltmaxalarm || battvalue < battvoltminalarm) {
+  if ((battvalue > battvoltmaxalarm) || (battvalue < battvoltminalarm))
+  {
     oldscreen = screen;
-    screen = 7;
+//    screen = 7;
     alarmreason = 4;
   }
-  else {
+  else
+  {
     alarmreason = 0;
   }    
   return battvalue;
 }
 
-void readrpm() {
+float readairtemp()
+{
+  int reading = analogRead(airsensorpin);  
+  float voltage = reading * 5;
+  voltage = voltage / 1024; 
+  float airtempvalue = (voltage - 0.5) * 100  ;
+  return airtempvalue;
+}  
+
+void readrpm()
+{
   rpm = (((rpmcount*4)*60)/2);
   rpmcount = 0;  
-return;
+  return;
 }
 
-void logtosdcard() {
-return;
+void logtosdcard()
+{
+  return;
 }
 
-void displayscreenhome() {
+void displayscreenhome()
+{
   char line0[17] = {"WT:      OT:    "};
   lcd.setCursor(0, 0);
   lcd.print(line0);
   char line1[17] = {"AT:      BV:    "};
   lcd.setCursor(0, 1);
   lcd.print(line1);
-  lcd.setCursor(13, 1);
-  lcd.print(battvolt);
-  lcd.setCursor(12, 1);
-  lcd.print(battvolt);
-  lcd.setCursor(14, 1);
-  lcd.print('.');
-return;
+  if ((airtemp > -40) && (airtemp < 125))
+  {
+    lcd.setCursor(3, 1);
+    lcd.print(airtemp,1);
+  }
+  if ((battvolt > 0) && (battvolt <20))
+  {
+    lcd.setCursor(12, 1);
+    lcd.print(battvolt,1);
+  }
+  return;
 }
 
-void displayscreenwater() {
-return;
+void displayscreenwater()
+{
+  return;
 }
 
-void displayscreenoil() {
-return;
+void displayscreenoil()
+{
+  return;
 }
 
-void displayscreenair() {
-return;
+void displayscreenair()
+{
+  return;
 }
 
-void displayscreenbatt() {
-return;
+void displayscreenbatt()
+{
+  return;
 }
 
-void displayscreenaccel() {
-return;
+void displayscreenaccel()
+{
+  return;
 }
 
-void displayscreenrpm() {
-return;
+void displayscreenrpm()
+{
+  return;
 }
 
-void displayscreenalarm() {
-return;
+void displayscreenalarm()
+{
+  return;
 }
 
-void displayleds(int revs) {
-return;
+void displayleds(int revs)
+{
+  return;
 }
 
-void tachpulse() {
-rpmcount++;
+void tachpulse()
+{
+  rpmcount++;
+  return;
 }
 
